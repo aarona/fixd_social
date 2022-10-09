3d33d1471b3cf67d2401ee8f14d939ffb4c3c18a
Aaron A <_aaron@tutanota.com>
2022-10-08 17:28:18 -0700

    Added CreatePost documentation and refactored code

--- app/services/create_post.rb
+++ app/services/create_post.rb
@@ -2,17 +2,27 @@ class CreatePost
   attr_reader :error_messages
   attr_reader :post
 
+  # Valid parameters are:
+  #   :user_id - The id of the user making the post
+  #   :title - The title of the post
+  #   :body - The body of the post
   def initialize(params)
     @params = params
     @error_messages = []
   end
 
   def save
-    @post = Post.new(@params.merge({ posted_at: DateTime.now }))
+    @post = Post.new(post_params)
 
     return true if @post.save
 
     @error_messages = @post.errors.full_messages
     false
-  end  
+  end
+
+  private
+
+  def post_params
+    @params.merge({ posted_at: DateTime.now })
+  end
 end
