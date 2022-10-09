31a259a40b2e6015dad1f28d735f357289b0ddfc
Aaron A <_aaron@tutanota.com>
2022-10-08 17:28:58 -0700

    Added CreateRating documentation and refactored code

--- app/services/create_rating.rb
+++ app/services/create_rating.rb
@@ -4,57 +4,77 @@ class CreateRating
   attr_reader :error_messages
   attr_reader :rating
 
+  # Valid parameters are:
+  #   :user_id - The id of the user being rated
+  #   :rater_id - The id of user leaving the rating
+  #   :rating - The rating; A integer between 1 and 5 (inclusive)
   def initialize(params)
     @params = params
     @error_messages = []
   end
   
   def save
-    user_id = @params[:user_id]
-    rater_id = @params[:rater_id]
-    rating_value = @params[:rating]
+    @user_id = @params[:user_id]
+    @rater_id = @params[:rater_id]
+    @rating_value = @params[:rating]
 
     begin
-      user = User.find(user_id)
+      @user = User.find(@user_id)
     rescue
       @error_messages = ["A user must be rated"]
+      return false
     end
 
-    return false if @error_messages.present?
-
-    if user_id == rater_id 
+    if @user_id == @rater_id 
       @error_messages = ["A user cannot rate themself"]
       return false
     end
 
-    if user.ratings.pluck(:rater_id).include? rater_id
-      @rating = Rating.where(user_id: user_id, rater_id: rater_id).first
-      current_average_rating = user.average_rating
-      @rating.update(rating: rating_value) if @rating.present?
+    return update_user_rating! if user_already_rated?
 
-      user.reload
-
-      if current_average_rating < RATING_THRESHOLD && user.average_rating >= RATING_THRESHOLD
-        create_rating_post!(user_id)
-      end
-    else
-      @rating = Rating.new(@params.merge({ rated_at: DateTime.now }))
-
-      unless @rating.save
-        @error_messages = @rating.errors.full_messages
-
-        return false
-      end
-
-      create_rating_post!(user_id) if rating_value >= RATING_THRESHOLD
-    end
-
-    true
+    create_user_rating!
   end
 
   private
 
-  def create_rating_post!(user_id)
-    CreatePost.new({ user_id: user_id, title: "You passed 4 stars!", body: "Congratulations!" }).save
+  def create_rating_post!
+    CreatePost.new({ user_id: @user_id, title: "You passed 4 stars!", body: "Congratulations!" }).save
+  end
+
+  def create_user_rating!
+    @rating = Rating.new(rating_params)
+
+    unless @rating.save
+      @error_messages = @rating.errors.full_messages
+
+      return false
+    end
+
+    create_rating_post! if @rating_value >= RATING_THRESHOLD
+
+    true
+  end
+
+  def rating_params
+    @params.merge({ rated_at: DateTime.now })
+  end
+  
+  def user_already_rated?
+    @user.ratings.pluck(:rater_id).include? @rater_id
+  end
+
+  def update_user_rating!
+    @rating = Rating.where(user_id: @user_id, rater_id: @rater_id).first
+    @current_average_rating = @user.average_rating
+    @rating.update(rating: @rating_value) if @rating.present?
+    @user.reload
+
+    create_rating_post! if user_met_rating_threshold?
+      
+    true
+  end
+
+  def user_met_rating_threshold?
+    @current_average_rating < RATING_THRESHOLD && @user.average_rating >= RATING_THRESHOLD
   end
 end
