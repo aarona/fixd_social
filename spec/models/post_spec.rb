d20b05b50127e490d6ca061def4d8db3a31382ec
Aaron A <_aaron@tutanota.com>
2022-10-11 14:58:48 -0700

    Added specs to ensure associated acitivity is also destroyed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -19,4 +19,32 @@ RSpec.describe Post, type: :model do
       expect(post.errors.full_messages).to include "User must exist"
     end
   end
+
+  context "when creating a post" do
+    let(:post) { build(:post) }
+
+    it "should have an associated activity with it" do
+      expect {
+        post.save
+      }.to change(Activity, :count).by(1)
+
+      activity = Activity.last
+
+      expect(post.id).to eq activity.loggable_id
+      expect(activity.loggable_type).to eq "Post"
+    end
+  end
+
+  context "when a post is destroyed" do
+    let(:post) { create(:post) }
+    let(:activity) { post.activity }
+
+    before do
+      post.destroy
+    end
+
+    it "the associated activity should also be deleted" do
+      expect(activity).to be_destroyed
+    end
+  end
 end
