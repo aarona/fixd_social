875ad59679cc0287303faba88f9edfacae6a1a5d
Aaron A <_aaron@tutanota.com>
2022-10-11 11:23:30 -0700

    Require loggable events to be deleted if the associated Activity is also deleted

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,8 +1,9 @@
 class Activity < ApplicationRecord
   belongs_to :user
-  belongs_to :loggable, polymorphic: true
+  belongs_to :loggable, polymorphic: true, dependent: :destroy
 
   default_scope { order("posted_at desc") }
+
 
   validates_presence_of :posted_at
 end
